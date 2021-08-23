cc909e1ad349a5e1bbab754b4cd9510a36bef87d
Arthur Vincent Simon <developarvin@gmail.com>
2021-08-23 11:47:40 +0800

    Declare prisma as a global

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  let prisma: PrismaClient | undefined;
 }
-
-let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
