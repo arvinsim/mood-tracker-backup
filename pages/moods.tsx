e8b5422e2ee69dc98689ca5a3fbe5e776541fa18
Arthur Vincent Simon <developarvin@gmail.com>
2021-08-23 11:46:34 +0800

    Refactor

--- pages/moods.tsx
+++ pages/moods.tsx
@@ -1,18 +1,13 @@
 import { GetServerSideProps } from "next";
 import prisma from "../lib/prisma";
 
-type Data = {
-  id: number;
-  name: string;
-};
-
-function Moods({ moods }) {
+function MoodsPage({ moods }) {
   return (
     <div>
       <h1>Moods</h1>
       <ul>
         {moods.map((mood) => {
-          return <li>{mood.name}</li>;
+          return <li key={mood.id}>{mood.name}</li>;
         })}
       </ul>
     </div>
@@ -20,10 +15,10 @@ function Moods({ moods }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const moods: Data[] = await prisma.mood.findMany();
+  const moods = await prisma.mood.findMany();
   return {
     props: { moods },
   };
 };
 
-export default Moods;
+export default MoodsPage;
