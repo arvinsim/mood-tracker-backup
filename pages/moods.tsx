1b9887589033474f15f4818c122798d961a96ed6
Arthur Vincent Simon <developarvin@gmail.com>
2021-08-23 17:07:34 +0800

    Fix issue with Typescript

--- pages/moods.tsx
+++ pages/moods.tsx
@@ -3,8 +3,15 @@ import { GetServerSideProps } from "next";
 import prisma from "../lib/prisma";
 
 type TMoods = Prisma.PromiseReturnType<typeof getMoods>;
+type TMoodLogs = Prisma.PromiseReturnType<typeof getMoodLogs>;
 
-function MoodsPage({ moods }: { moods: TMoods }) {
+function MoodsPage({
+  moods,
+  moodLogs,
+}: {
+  moods: TMoods;
+  moodLogs: TMoodLogs;
+}) {
   return (
     <div>
       <h1>Moods</h1>
@@ -13,19 +20,29 @@ function MoodsPage({ moods }: { moods: TMoods }) {
           return <li key={mood.id}>{mood.name}</li>;
         })}
       </ul>
+      <ul>
+        {moodLogs.map((moodLog) => {
+          return <li key={moodLog.id}>{moodLog.createdAt}</li>;
+        })}
+      </ul>
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const moods = await getMoods();
+  const moodLogs = await getMoodLogs();
   return {
-    props: { moods },
+    props: { moods, moodLogs },
   };
 };
 
 export const getMoods = async () => {
   return await prisma.mood.findMany();
+};
+
+export const getMoodLogs = async () => {
+  return await prisma.moodLog.findMany();
 };
 
 export default MoodsPage;
