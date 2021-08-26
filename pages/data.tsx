4a323adfeffb70b45e04e2b679e4db6644c9743e
Arthur Vincent Simon <developarvin@gmail.com>
2021-08-27 00:59:39 +0800

    Serialize date

--- pages/data.tsx
+++ pages/data.tsx
@@ -1,28 +1,34 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import prisma from "../lib/prisma";
 
 import type { MoodLog } from "@prisma/client";
-import type { NextPage } from "next";
 
-const DataPage: NextPage<{ moodLogs: MoodLog[] }> = ({ moodLogs }) => {
+function DataPage({
+  moodLogs,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       <h1>These is how you have been feeling this week!</h1>
       <div>
         {moodLogs.map((moodLog) => {
-          return <div key={moodLog.id}>{moodLog.moodId}</div>;
+          return (
+            <div key={moodLog.id}>
+              {moodLog.moodId} - {moodLog.createdAt.getTime()}
+            </div>
+          );
         })}
       </div>
     </div>
   );
-};
+}
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const moodLogs = await getMoodLogs();
-  return {
-    props: { moodLogs },
+export const getServerSideProps: GetServerSideProps<{ moodLogs: MoodLog[] }> =
+  async () => {
+    const moodLogs = await getMoodLogs();
+    return {
+      props: { moodLogs },
+    };
   };
-};
 
 export const getMoodLogs = async () => {
   return await prisma.moodLog.findMany();
